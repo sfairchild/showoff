cda8644fc5e61bd6ea6295abd03532d6d45d3759
Sean <seanf215@gmail.com>
2019-10-05 02:33:27 -0400

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Showoff.MixProject do
     [
       app: :showoff,
       version: "0.2.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
