11b3c98db4d5f80dd7c17303893220c171a4a15f
Sean <seanf215@gmail.com>
2019-10-05 02:39:00 -0400

    readd elixir 1.9 and use release

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Showoff.MixProject do
     [
       app: :showoff,
       version: "0.2.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
